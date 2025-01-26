aae548e8f1e3a77373fdd5a900e6ce45db57255b
Eric Moura <124516716+Wr3tchedTorch@users.noreply.github.com>
2025-01-25 21:16:22 -0300

    add basic player movement

---
+++ scenes/bullet/WeakBullet.cs
@@ -0,0 +1,11 @@
+using Godot;
+
+namespace Game.Bullet;
+
+public partial class WeakBullet : Area2D
+{
+
+	public override void _Ready()
+	{
+	}
+}
---
+++ scenes/player/Player.cs
@@ -0,0 +1,52 @@
+using Godot;
+
+namespace Game.Player;
+
+public partial class Player : CharacterBody2D
+{
+    private readonly StringName actionJump  = "jump";
+    private readonly StringName actionLeft  = "left";
+    private readonly StringName actionRight = "right";
+    private readonly StringName actionShoot = "shoot";
+
+    [Export] private float speed  =  300.0f;
+    
+    [ExportGroup("Jump")]
+    [Export] private float height = 10_000.0f;
+    [Export] private float duration = 30f;
+
+    private float Gravity   => 8 * height / (duration*duration);
+    private float JumpForce => Mathf.Sqrt(2 * height * Gravity);
+
+    private Vector2 toVelocity;
+    
+    public override void _PhysicsProcess(double delta)
+    {        
+        if (IsOnFloor()) 
+        {
+            toVelocity.Y = 0;
+        }
+
+        HorizontalMovement();
+
+        toVelocity.Y += Gravity;
+        if (Input.IsActionJustPressed(actionJump)) 
+        {
+            Jump();
+        }
+        
+        Velocity = toVelocity;
+        MoveAndSlide();
+    }    
+
+    private void HorizontalMovement() 
+    {
+        float xDirection = Input.GetAxis(actionLeft, actionRight);
+        toVelocity.X = xDirection * speed;
+    }    
+
+    private void Jump() 
+    {        
+        toVelocity.Y = -JumpForce;
+    }
+}
